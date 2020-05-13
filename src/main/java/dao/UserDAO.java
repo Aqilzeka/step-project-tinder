23cd9e059f69a3944765a3166416738c50bbe505
Aqil <41694337+Aqilzeka@users.noreply.github.com>
2020-05-13 18:08:31 +0400

    Updated

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -12,9 +12,9 @@ import java.util.List;
 public class UserDAO implements DAO<User> {
 
 
-    private final static String URL = "jdbc:postgresql://localhost:5432/tinder";
-    private final static String NAME = "postgres";
-    private final static String PASSWORD = "1999Aqil";
+    private final static String URL = "jdbc:postgresql://ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/d36isdpvdr1253";
+    private final static String NAME = "mgfwnnvgwivxck";
+    private final static String PASSWORD = "a7563aa462d5ab9290ca6c5ac6fdb0932487fe94b10adcc13db9690fe0c55f10";
 
     public static final String INSERT_EMAIL_PSW =
             "INSERT INTO users(email, password) values(?,?);";
